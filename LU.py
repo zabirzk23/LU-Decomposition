caafe16deb7072bafd54fd93f9b9bbbca5b27914
AkilaMohan <akilamohan@saveetha.ac.in>
2024-01-10 12:39:25 +0530

    final

---
+++ LU.py
@@ -0,0 +1,14 @@
+# To print L and U matrix
+import numpy as np
+#add the library to import lu
+A = np.array(eval(input()))
+#add the code to get the L and U matrix
+
+
+# To print X matrix (solution to the equations)
+import numpy as np
+#add the library to import lu_factor, lu_solve
+A = np.array(eval(input()))
+b = np.array(eval(input()))
+#add the code to get the solution of the matrix
+
